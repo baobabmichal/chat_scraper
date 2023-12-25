b49a4d76d4e3bb1f41944f864bea83ce7e594ade
baobabmichal <baobabmichal@gmail.com>
2023-12-25 20:05:23 +0100

    add click more function

--- packages/common/data_model.py
+++ packages/common/data_model.py
@@ -1,3 +1,4 @@
+import enum
 from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
@@ -6,6 +7,13 @@ from selenium.webdriver.remote.webelement import WebElement
 
 class CustomModel(BaseModel):
     model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
+
+
+class TabListStatus(enum.Enum):
+    all_visible = "all_visible"
+    one_hidden = "one_hidden"
+    more_hidden_list_not_visible = "more_hidden_list_not_visible"
+    more_hidden_list_visible = "more_hidden_list_visible"
 
 
 class Room(CustomModel):
